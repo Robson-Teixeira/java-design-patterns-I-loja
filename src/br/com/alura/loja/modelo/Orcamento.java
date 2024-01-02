1ed597957a5341a62b068058da0098ff828a48b8
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-01 22:56:37 -0300

    State: Adicionando a situação do orçamento

--- src/br/com/alura/loja/modelo/Orcamento.java
+++ src/br/com/alura/loja/modelo/Orcamento.java
@@ -6,6 +6,7 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private int quantidadeItens;
+	private String situacao;
 
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
@@ -18,6 +19,23 @@ public class Orcamento {
 
 	public int getQuantidadeItens() {
 		return quantidadeItens;
+	}
+
+	public void aprovar() {
+		this.situacao = "Aprovado";
+	}
+
+	public void aplicarDescontoExtra() {
+
+		BigDecimal valorDescontoExtra = BigDecimal.ZERO;
+
+		if (situacao.equals("Em Análise"))
+			valorDescontoExtra = new BigDecimal("0.05");
+		else if (situacao.equals("Aprovado"))
+			valorDescontoExtra = new BigDecimal("0.02");
+
+		this.valor = this.valor.subtract(valorDescontoExtra);
+
 	}
 
 }
